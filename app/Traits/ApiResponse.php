2bb1b2ce44c660b609dba2da3ae11f8fd0ac82c1
amirsahra <amirhosein.sahra@gmail.com>
2022-11-15 10:36:54 +0330

    refactor: remove array type for data parameter #9

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -6,7 +6,7 @@ use Illuminate\Http\JsonResponse;
 
 trait ApiResponse
 {
-    protected function apiResult(string $message, array $data = null, bool $success = true, int $status = 200): JsonResponse
+    protected function apiResult(string $message, $data = null, bool $success = true, int $status = 200): JsonResponse
     {
         return response()->json([
             'success' => $success,
