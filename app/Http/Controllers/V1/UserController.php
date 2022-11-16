ee4d95066b577ba0a4fd2139955d6e1a56c00ce9
amirsahra <amirhosein.sahra@gmail.com>
2022-11-16 11:47:43 +0330

    feat: user destroy method #2

--- app/Http/Controllers/V1/UserController.php
+++ app/Http/Controllers/V1/UserController.php
@@ -46,14 +46,10 @@ class UserController extends Controller
         );
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return $this->apiResult(__('messages.destroy_method',['name'=>__('values.user')]),
+        );
     }
 }
--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -5,5 +5,6 @@ return [
     'store_method' => ':name create successfully.',
     'show_method' => 'show :name',
     'update_method' => 'update :name successfully.',
+    'destroy_method' => 'delete :name successfully.',
     'validate_failed' => 'The given data was invalid.',
 ];
