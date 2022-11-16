0ff7128bf2c727ec576680bfd2962dba695a0d89
amirsahra <amirhosein.sahra@gmail.com>
2022-11-16 13:04:40 +0330

    feat: member middleware

--- app/Http/Middleware/Member.php
+++ app/Http/Middleware/Member.php
@@ -2,20 +2,25 @@
 
 namespace App\Http\Middleware;
 
+use App\Traits\ApiResponse;
 use Closure;
 use Illuminate\Http\Request;
 
 class Member
 {
+    use ApiResponse;
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\JsonResponse
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if (auth()->check() && auth()->user()->type = 'member')
+            return $next($request);
+        return $this->apiResult(__('messages.forbidden'), null, false, 403);
     }
 }
