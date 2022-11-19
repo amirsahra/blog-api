45c8236384e642b0535dff3c84a9f70d22840196
amirsahra <amirhosein.sahra@gmail.com>
2022-11-19 12:56:47 +0330

    feat: category resource

--- app/Http/Resources/V1/CategoryResource.php
+++ app/Http/Resources/V1/CategoryResource.php
@@ -9,11 +9,19 @@ class CategoryResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @param \Illuminate\Http\Request $request
+     * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'children' => CategoryResource::collection(
+                $this->whenLoaded('children'))
+        ];
     }
 }
