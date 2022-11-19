584242795662acfc7644bc5376cd537b86ddabf7
amirsahra <amirhosein.sahra@gmail.com>
2022-11-19 13:31:38 +0330

    feat: category children

---
+++ app/Http/Resources/V1/CategoryChildrenResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryChildrenResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'parent_id' => $this->parent_id,
+            'children' => CategoryChildrenResource::collection(
+                $this->whenLoaded('children'))
+        ];
+    }
+}
